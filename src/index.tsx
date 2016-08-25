70e6d16568e5e9382d227bbeab781488f6cbfd60
huangziyi01 <huangziyi01@baidu.com>
2016-08-25 10:41:34 +0800

    quick push

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+/// <reference path="../../../../typings/tsd.d.ts" />
+/// <reference path="../../gaea/models/tsd.d.ts" />
+
+import Preview from './preview/preview.component'
+
+export default Preview
